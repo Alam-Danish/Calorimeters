ef58f6f3a449e5e129cf227edd3f3ab981e843b7
Alam-Danish <danish.alam@studio.unibo.it>
2023-07-03 17:12:05 +0200

    EM Calorimeter added

--- construction.cc
+++ construction.cc
@@ -25,5 +25,13 @@ G4VPhysicalVolume *MyDetectorConstruction::Construct()
     // Physical volume of the world volume with logical volume, rotation, position, logical volume, mother volume name, boolean operation, copy number, check overlapes
     G4VPhysicalVolume *worldPhysical = new G4PVPlacement(0, G4ThreeVector(0., 0., 0.), worldLogical, "worldPhysical", 0, false, 0, true);
 
+    // EM Calorimeter volume material
+    G4Material *CsI = nist->FindOrBuildMaterial("G4_CESIUM_IODIDE"); // Cesium Iodide
+
+    // EM Calorimeter volume
+    G4VSolid *emCalSolid = new G4Box("emCalSolid", 1.5*m, 30.*cm, 15.*cm);
+    G4LogicalVolume *emCalLogical = new G4LogicalVolume(emCalSolid, CsI, "emCalLogical");
+    G4VPhysicalVolume *emCalPhysical = new G4PVPlacement(0, G4ThreeVector(0., 0., 2.*m), emCalLogical, "emCalPhysical", worldLogical, false, 0, true);
+
     return worldPhysical;
 }
